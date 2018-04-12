19a29e89c5c486cb91f3e3068332291e7fd9ae79
pineshmenat <pineshmenat@gmail.com>
2018-04-11 22:27:12 -0400

    Add project files.

---
+++ ImageDownloadHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+namespace ImageDownloadHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (System.ServiceModel.ServiceHost host = new System.ServiceModel.ServiceHost(typeof(ImageDownloadServiceLibrary.ImageDownloadService)))
+            {
+                host.Open();
+                Console.WriteLine("Host Started, Press any key to stop...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ImageDownloadServiceLibrary/ImageDownloadService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ImageDownloadServiceLibrary
+{
+    public class ImageDownloadService : IImageDownloadService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+        
+    }
+}
